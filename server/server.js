db1f3085741d7aff1ea022b777901ff9d60a0d3e
sota-f625 <s7716xf625@gmail.com>
2024-11-30 13:24:00 +0900

    サーバー作成

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
