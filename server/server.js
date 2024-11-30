5cd8c4e543cf981f734fd9686dbf6ff2d390080e
sota-f625 <s7716xf625@gmail.com>
2024-11-30 13:28:06 +0900

    ポートの変更

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send("Server is running");
