18c1e86d773d221ea4062e714d225aa877f30dc6
sota-f625 <s7716xf625@gmail.com>
2024-11-30 14:07:41 +0900

    FE,BE連携確認

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+
+const App = () => {
+  useEffect(() => {
+    axios.get("http://localhost:5001/")
+      .then(response => console.log(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = "App">
+      <header className = "App-header">
+        <img src = {logo} className = "App-logo" alt = "logo" />
+        <h1>ECat</h1>
       </header>
     </div>
   );
-}
+};
 
 export default App;
